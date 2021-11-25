b31b7c7ddd96c603447ebf7cf5fb1c13b7b606bd
Iris <iris_chan_77@hotmail.com>
2021-11-24 20:33:07 -0500

    Get data from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 function App() {
+  axios({
+    url: 'https://opentdb.com/api.php?amount=10',
+    method: 'GET',
+    dataResponse: 'json'
+  }).then((res) => {
+    console.log(res.data.results);
+  });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Quarantine Pursuit</h1>
+        <p>See console</p>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
